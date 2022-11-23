e8575f562beba9da8e5a216a7e13f942aeb4691b
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-11-24 01:21:56 +0530

    added routes boiler plate

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const { connectDatabase } = require('./Database/dbConnect');
 const { Cart } = require('./models/cart.model');
 const { Product } = require('./models/product.models');
 const { User } = require('./models/user.model');
+const { cartRouter } = require('./routes/cart.router');
+const { orderRouter } = require('./routes/order.router');
+const { productsRouter } = require('./routes/products.router');
+const { userRouter } = require('./routes/user.router');
 const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = 8080;
 
 
 
 
-connectDatabase().then(async ()=>{
+app.use("/user",userRouter);
+app.use("/cart",cartRouter);
+app.use("/orders",orderRouter);
+app.use("/products",productsRouter);
+
+
+
+
+connectDatabase().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`listening on ${PORT}`);
+    })
     
 })
 
---
+++ routes/products.router.jsx
@@ -0,0 +1,10 @@
+const express = require('express');
+const productsRouter = express.Router();
+
+
+productsRouter.get('/',)
+productsRouter.get('/:id',)
+
+
+
+module.exports = {productsRouter}
