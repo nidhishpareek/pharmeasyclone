af21d90b20ed46c2da72d43d12b773d0f156a09c
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-11-24 20:10:57 +0530

    products api

---
+++ controllers/products.controller.js
@@ -0,0 +1,81 @@
+const { Product} = require("../models/product.models");
+
+
+
+async function getCategories(req,res){
+    try {
+
+        let totalCategories = await Product.distinct("sub_category")
+
+        totalCategories = totalCategories.filter(el=>{
+            return (el==="Baby Care" || el==="Cleansers" || el==="Diabetic Care" || el==="Dressing" ||
+            el==="Massage Tools" || el==="Homeopathy" || el==="Vitamins And Supplements" || el==="Maternity Care" || el==='Hair Styling Tools') ;
+        })
+         res.send({totalCategories})
+        
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
+}
+
+async function getAllProductsByCategory(req, res) {
+
+    try {
+        let {category} = req.params;
+        let {
+            brand = '',
+            search = '',
+            pageSize = 20, 
+            page = 1,
+            sortBy = '_id',
+            sortOrder = '' 
+        } = req.query;
+    
+        const totalProducts = await Product.find({
+            sub_category:category,
+        }).count();
+        if(totalProducts === 0) {
+           return  res.status(404).send({message:"Page not found"}) 
+        }
+        
+    
+        const products = await Product.find({
+            sub_category:category,
+        })
+        .sort({
+            [sortBy]: sortOrder === 'asc' ? 1 : -1
+        })
+        .limit(pageSize)
+        .skip(pageSize * (page - 1));
+    
+        return res.send({
+                totalProducts,
+                products,
+                page,
+                pageSize    
+        })
+    } catch(err) {
+        return res.status(500).send({
+            status: 'error',
+            message: 'Something went wrong'
+        })
+    }
+}
+
+async function getProductById(req, res) {
+
+    const {id} = req.params;
+
+    const product = await Product.find({
+        '_id': id,
+    })
+
+    return res.send({
+       product
+    })
+}
+
+module.exports={
+    getAllProductsByCategory,
+    getProductById,getCategories
+}
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const { connectDatabase } = require('./Database/dbConnect');
 const { Cart } = require('./models/cart.model');
 const { Product } = require('./models/product.models');
 const { User } = require('./models/user.model');
+const { productsRouter } = require('./routes/products.router');
 // const { cartRouter } = require('./routes/cart.router');
 // const { orderRouter } = require('./routes/order.router');
-// const { productsRouter } = require('./routes/products.router');
+
 // const { userRouter } = require('./routes/user.router');
 const app = express();
 app.use(express.json());
@@ -20,34 +21,20 @@ const PORT = 8080;
 // app.use("/user",userRouter);
 // app.use("/cart",cartRouter);
 // app.use("/orders",orderRouter);
-// app.use("/products",productsRouter);
+   app.use("/products",productsRouter);
 
-async function cart  (){
+
+
+
+
+
+
+connectDatabase().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`listening on ${PORT}`);
+    })
     
-    // const user = await  Cart.create({
-    //     userId:'637e6071895d22ca7a477f50',
-    //     cartItems:[{
-    //         productId:'637e45bbe056124f16be5c9d',
-    //         quantity:1,
-    //         discount:10,
-
-    //     }]
-    // });
-    console.log(await Cart.find());
-
-}
-cart();
-
-
-connectDatabase();
-
-
-// connectDatabase().then(()=>{
-//     app.listen(PORT,()=>{
-//         console.log(`listening on ${PORT}`);
-//     })
-    
-// })
+})
 
 
 
