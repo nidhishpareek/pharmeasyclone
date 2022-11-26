91624b06b5b96c29b94c88ccaceba4b51a581c8f
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-11-26 20:52:00 +0530

    folder fixes

--- server/index.js
+++ server/index.js
@@ -2,16 +2,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser')
 const express = require('express');
 const mongoose = require('mongoose');
-const { connectDatabase } = require('./Database/dbConnect');
+const { connectDatabase } = require('../Database/dbConnect');
 const { Cart } = require('./models/cart.model');
 const { Product } = require('./models/product.models');
 const { User } = require('./models/user.model');
-const { productsRouter } = require('./routes/products.routes');
-const { cartRouter } = require('./routes/cart.routes');
+const { productsRouter } = require('../routes/products.routes');
+const { cartRouter } = require('../routes/cart.routes');
 // const { orderRouter } = require('./routes/order.routes');
 
-const { userRouter } = require('./routes/user.routes');
-const { authMiddleware } = require('./middlewares/auth');
+const { userRouter } = require('../routes/user.routes');
+const { authMiddleware } = require('../middlewares/auth');
 const app = express();
 app.use(express.json());
 app.use(cors());
